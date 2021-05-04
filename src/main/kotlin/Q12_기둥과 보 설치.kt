c7cd5d6dd59fefce0d875324cc029c14200b9ffb
hhs823 <42017905+hhs823@users.noreply.github.com>
2021-05-04 14:44:38 +0900

    update

---
+++ src/main/kotlin/Q12_기둥과 보 설치.kt
@@ -0,0 +1,41 @@
+﻿class Solution2 {
+    fun solution(n: Int, build_frame: Array<IntArray>): Array<IntArray> {
+        var answer = mutableListOf<IntArray>()
+
+        for (frame in build_frame) {
+            val (x, y, stuff, operate) = frame
+            if (operate == 0) {
+                val tests = answer.filter { it[0] != x || it[1] != y || it[2] != stuff }.toMutableList()
+                var isDelete = true
+                for (test in tests) {
+                    if (!possible(tests, test[0], test[1], test[2])) {
+                        isDelete = false
+                        break
+                    }
+                }
+                if (isDelete) answer = tests
+            } else {
+                if (possible(answer, x, y, stuff)) answer.add(intArrayOf(x, y, stuff))
+            }
+        }
+        val edges:Array<IntArray> = arrayOf()
+        return answer.sortedWith(compareBy({ it[0] }, { it[1] }, { it[2] })).toTypedArray()
+    }
+
+    fun possible(answer: MutableList<IntArray>, x: Int, y: Int, stuff: Int): Boolean {
+        if (stuff == 0) {
+            if (y == 0)
+                return true
+            if (answer.any { it[2] == 0 && it[1] == y - 1 && it[0] == x })
+                return true
+            if (answer.any { it[2] == 1 && it[1] == y && (it[0] == x - 1 || it[0] == x) })
+                return true
+        } else {
+            if (answer.any { it[2] == 0 && (it[1] == y - 1) && (it[0] == x || it[0] == x + 1) })
+                return true
+            if (answer.filter { it[2] == 1 && (it[1] == y) && (it[0] == x - 1 || it[0] == x + 1) }.size == 2)
+                return true
+        }
+        return false
+    }
+}
---
+++ src/main/kotlin/Q14_외벽 점검.kt
@@ -0,0 +1,36 @@
+﻿fun main() {
+    class Solution {
+        fun solution(n: Int, weak: IntArray, dist: IntArray): Int {
+            // 원 형태를 길이를 2배로 늘린 배열로 변형
+            var extended = weak + IntArray(weak.size) { weak[it] + n }
+
+            var answer = dist.size + 1
+            // 가능한 모든 순열
+            val permutation = permutation(dist.toList())
+            // 모든 시작 위치에 대해서
+            for (start in weak.indices) {
+                for (friends in permutation) {
+                    var count = 1
+                    var position = extended[start] + friends[count - 1]
+                    for (index in start until start + weak.size) {
+                        // 점검 위치를 초과한 경우
+                        if (position < extended[index]) {
+                            count += 1
+                            if (count > dist.size) break // 모든 사람을 투입한 경우
+                            position = extended[index] + friends[count - 1]
+                        }
+                    }
+                    if (answer > count) answer = count
+                }
+            }
+            if (answer > dist.size) return -1
+            return answer
+        }
+
+        fun <T> permutation(el: List<T>, fin: List<T> = listOf(), sub: List<T> = el): List<List<T>> =
+            if (sub.isEmpty()) listOf(fin)
+            else sub.flatMap { permutation(el, fin + it, sub - it) }
+
+
+    }
+}
